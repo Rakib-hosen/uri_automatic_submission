ac14eec9b20e1cb46fa024c58505875f8ce538d0
Rakib Hosen <rakibhosen52970@gmail.com>
2020-08-14 00:03:21 +0600

    Update uri.py

--- uri.py
+++ uri.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support.select import Select
 import time
-user = "humanphysiology06@gmail.com"
-pwd = "11111111"
+user = ""
+pwd = ""
 class uri:
     def __init__(self,user,pwd):
         cap = DesiredCapabilities().FIREFOX
